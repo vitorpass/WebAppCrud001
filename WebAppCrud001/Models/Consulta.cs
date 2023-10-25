e48847b915484ee2ce195e4a434131d5505daf60
vitorpass <vitorpp2005.vpp@gmail.com>
2023-10-25 15:34:08 -0300

    Aula 25/10/2023

---
+++ WebAppCrud001/Models/Consulta.cs
@@ -0,0 +1,9 @@
+﻿namespace WebAppCrud001.Models
+{
+    public class Consulta
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public string Sobrenome { get; set; }
+    }
+}
---
+++ WebAppCrud001/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace WebAppCrud001.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
